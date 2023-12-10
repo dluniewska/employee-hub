a14e878cb01b9a49bfc2d4ca9d7d9e2aec515c28
Daria Łuniewska <luniewska.d@gmail.com>
2023-12-10 20:29:45 +0100

    updated initial migration with default date values, changed user - competence relation to many-to-many

--- apps/api/prisma/migrations/20231210192658_initial/migration.sql
+++ apps/api/prisma/migrations/20231210192658_initial/migration.sql
@@ -10,8 +10,8 @@ CREATE TABLE "User" (
     "positionId" INTEGER NOT NULL,
     "unitId" INTEGER NOT NULL,
     "description" VARCHAR(250) NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL,
-    "createdBy" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "createdBy" TEXT NOT NULL,
     "updatedAt" TIMESTAMP(3),
     "updatedBy" TEXT,
     "deletedAt" TIMESTAMP(3),
@@ -26,8 +26,8 @@ CREATE TABLE "Position" (
     "id" SERIAL NOT NULL,
     "name" VARCHAR(100) NOT NULL,
     "description" VARCHAR(200) NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL,
-    "createdBy" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "createdBy" TEXT NOT NULL,
     "updatedAt" TIMESTAMP(3),
     "updatedBy" TEXT,
     "deletedAt" TIMESTAMP(3),
@@ -45,8 +45,8 @@ CREATE TABLE "Experience" (
     "duration" INTEGER NOT NULL,
     "userId" BIGINT NOT NULL,
     "isPrivate" BOOLEAN NOT NULL DEFAULT false,
-    "createdAt" TIMESTAMP(3) NOT NULL,
-    "createdBy" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "createdBy" TEXT NOT NULL,
     "updatedAt" TIMESTAMP(3),
     "updatedBy" TEXT,
     "deletedAt" TIMESTAMP(3),
@@ -60,9 +60,8 @@ CREATE TABLE "Experience" (
 CREATE TABLE "Competence" (
     "id" BIGSERIAL NOT NULL,
     "name" VARCHAR(100) NOT NULL,
-    "userId" BIGINT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL,
-    "createdBy" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "createdBy" TEXT NOT NULL,
     "updatedAt" TIMESTAMP(3),
     "updatedBy" TEXT,
     "deletedAt" TIMESTAMP(3),
@@ -77,8 +76,8 @@ CREATE TABLE "Unit" (
     "id" SERIAL NOT NULL,
     "name" VARCHAR(100),
     "parentId" INTEGER,
-    "createdAt" TIMESTAMP(3) NOT NULL,
-    "createdBy" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "createdBy" TEXT NOT NULL,
     "updatedAt" TIMESTAMP(3),
     "updatedBy" TEXT,
     "deletedAt" TIMESTAMP(3),
@@ -94,8 +93,8 @@ CREATE TABLE "Schedule" (
     "status" "Status" NOT NULL,
     "date" TIMESTAMP(3) NOT NULL,
     "userId" BIGINT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL,
-    "createdBy" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "createdBy" TEXT NOT NULL,
     "updatedAt" TIMESTAMP(3),
     "updatedBy" TEXT,
     "deletedAt" TIMESTAMP(3),
@@ -105,8 +104,23 @@ CREATE TABLE "Schedule" (
     CONSTRAINT "Schedule_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "_CompetenceToUser" (
+    "A" BIGINT NOT NULL,
+    "B" BIGINT NOT NULL
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Position_name_key" ON "Position"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_CompetenceToUser_AB_unique" ON "_CompetenceToUser"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_CompetenceToUser_B_index" ON "_CompetenceToUser"("B");
 
 -- AddForeignKey
 ALTER TABLE "User" ADD CONSTRAINT "User_positionId_fkey" FOREIGN KEY ("positionId") REFERENCES "Position"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -118,10 +132,13 @@ ALTER TABLE "User" ADD CONSTRAINT "User_unitId_fkey" FOREIGN KEY ("unitId") REFE
 ALTER TABLE "Experience" ADD CONSTRAINT "Experience_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Competence" ADD CONSTRAINT "Competence_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
 ALTER TABLE "Unit" ADD CONSTRAINT "Unit_parentId_fkey" FOREIGN KEY ("parentId") REFERENCES "Unit"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Schedule" ADD CONSTRAINT "Schedule_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_CompetenceToUser" ADD CONSTRAINT "_CompetenceToUser_A_fkey" FOREIGN KEY ("A") REFERENCES "Competence"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_CompetenceToUser" ADD CONSTRAINT "_CompetenceToUser_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;

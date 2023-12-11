e332ddb3338b7f27a44c6c5c462e8deb2c91cb69
Daria Łuniewska <luniewska.d@gmail.com>
2023-12-11 21:18:32 +0100

    updated schema with explicit many-to-many table, updated seed with create method and separate objects so its more flexible, added clearing tables on top

--- apps/api/prisma/migrations/20231211201136_initial/migration.sql
+++ apps/api/prisma/migrations/20231211201136_initial/migration.sql
@@ -2,7 +2,7 @@
 CREATE TYPE "Status" AS ENUM ('ONSITE', 'REMOTE', 'DELEGATION', 'ABSENT');
 
 -- CreateTable
-CREATE TABLE "User" (
+CREATE TABLE "Users" (
     "id" BIGSERIAL NOT NULL,
     "email" VARCHAR(75) NOT NULL,
     "firstname" VARCHAR(75),
@@ -18,11 +18,36 @@ CREATE TABLE "User" (
     "deletedBy" TEXT,
     "deleted" BOOLEAN NOT NULL DEFAULT false,
 
-    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Users_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "Position" (
+CREATE TABLE "Skills" (
+    "id" BIGSERIAL NOT NULL,
+    "name" VARCHAR(100) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "createdBy" TEXT NOT NULL,
+    "updatedAt" TIMESTAMP(3),
+    "updatedBy" TEXT,
+    "deletedAt" TIMESTAMP(3),
+    "deletedBy" TEXT,
+    "deleted" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "Skills_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "SkillsOnUsers" (
+    "skillId" BIGINT NOT NULL,
+    "userId" BIGINT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "createdBy" TEXT NOT NULL,
+
+    CONSTRAINT "SkillsOnUsers_pkey" PRIMARY KEY ("skillId","userId")
+);
+
+-- CreateTable
+CREATE TABLE "Positions" (
     "id" SERIAL NOT NULL,
     "name" VARCHAR(100) NOT NULL,
     "description" VARCHAR(200) NOT NULL,
@@ -34,7 +59,7 @@ CREATE TABLE "Position" (
     "deletedBy" TEXT,
     "deleted" BOOLEAN NOT NULL DEFAULT false,
 
-    CONSTRAINT "Position_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Positions_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -58,22 +83,7 @@ CREATE TABLE "Experience" (
 );
 
 -- CreateTable
-CREATE TABLE "Competence" (
-    "id" BIGSERIAL NOT NULL,
-    "name" VARCHAR(100) NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "createdBy" TEXT NOT NULL,
-    "updatedAt" TIMESTAMP(3),
-    "updatedBy" TEXT,
-    "deletedAt" TIMESTAMP(3),
-    "deletedBy" TEXT,
-    "deleted" BOOLEAN NOT NULL DEFAULT false,
-
-    CONSTRAINT "Competence_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "Unit" (
+CREATE TABLE "Units" (
     "id" SERIAL NOT NULL,
     "name" VARCHAR(100),
     "parentId" INTEGER,
@@ -85,11 +95,11 @@ CREATE TABLE "Unit" (
     "deletedBy" TEXT,
     "deleted" BOOLEAN NOT NULL DEFAULT false,
 
-    CONSTRAINT "Unit_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Units_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "Schedule" (
+CREATE TABLE "Schedules" (
     "id" BIGSERIAL NOT NULL,
     "status" "Status" NOT NULL,
     "date" TIMESTAMP(3) NOT NULL,
@@ -102,47 +112,38 @@ CREATE TABLE "Schedule" (
     "deletedBy" TEXT,
     "deleted" BOOLEAN NOT NULL DEFAULT false,
 
-    CONSTRAINT "Schedule_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "_CompetenceToUser" (
-    "A" BIGINT NOT NULL,
-    "B" BIGINT NOT NULL
+    CONSTRAINT "Schedules_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+CREATE UNIQUE INDEX "Users_email_key" ON "Users"("email");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Position_name_key" ON "Position"("name");
+CREATE UNIQUE INDEX "Skills_name_key" ON "Skills"("name");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Competence_name_key" ON "Competence"("name");
+CREATE UNIQUE INDEX "Positions_name_key" ON "Positions"("name");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "_CompetenceToUser_AB_unique" ON "_CompetenceToUser"("A", "B");
-
--- CreateIndex
-CREATE INDEX "_CompetenceToUser_B_index" ON "_CompetenceToUser"("B");
+CREATE UNIQUE INDEX "Units_name_key" ON "Units"("name");
 
 -- AddForeignKey
-ALTER TABLE "User" ADD CONSTRAINT "User_positionId_fkey" FOREIGN KEY ("positionId") REFERENCES "Position"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Users" ADD CONSTRAINT "Users_positionId_fkey" FOREIGN KEY ("positionId") REFERENCES "Positions"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "User" ADD CONSTRAINT "User_unitId_fkey" FOREIGN KEY ("unitId") REFERENCES "Unit"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Users" ADD CONSTRAINT "Users_unitId_fkey" FOREIGN KEY ("unitId") REFERENCES "Units"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Experience" ADD CONSTRAINT "Experience_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "SkillsOnUsers" ADD CONSTRAINT "SkillsOnUsers_skillId_fkey" FOREIGN KEY ("skillId") REFERENCES "Skills"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Unit" ADD CONSTRAINT "Unit_parentId_fkey" FOREIGN KEY ("parentId") REFERENCES "Unit"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "SkillsOnUsers" ADD CONSTRAINT "SkillsOnUsers_userId_fkey" FOREIGN KEY ("userId") REFERENCES "Users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Schedule" ADD CONSTRAINT "Schedule_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Experience" ADD CONSTRAINT "Experience_userId_fkey" FOREIGN KEY ("userId") REFERENCES "Users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "_CompetenceToUser" ADD CONSTRAINT "_CompetenceToUser_A_fkey" FOREIGN KEY ("A") REFERENCES "Competence"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "Units" ADD CONSTRAINT "Units_parentId_fkey" FOREIGN KEY ("parentId") REFERENCES "Units"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "_CompetenceToUser" ADD CONSTRAINT "_CompetenceToUser_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "Schedules" ADD CONSTRAINT "Schedules_userId_fkey" FOREIGN KEY ("userId") REFERENCES "Users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- apps/api/prisma/seed.ts
+++ apps/api/prisma/seed.ts
@@ -5,60 +5,131 @@ import * as moment from "moment";
 const prisma = new PrismaClient();
 
 async function main() {
-    // seed for tests
 
-    const user1 = await prisma.user.upsert({
-        where: { email: 'j.violin@test.com' },
-        update: {},
-        create: {
+    await prisma.user.deleteMany({})
+    await prisma.unit.deleteMany({})
+    await prisma.position.deleteMany({})
+    await prisma.experience.deleteMany({})
+    await prisma.skill.deleteMany({})
+    await prisma.skillsOnUsers.deleteMany({})
+    await prisma.schedule.deleteMany({})
+    
+    // seed for tests, in separate objects for db provider flexibility
+    const skillDotnet = await prisma.skill.create({
+        data: {
+            id: 1,
+            name: ".NET",
+            createdBy: "seed"
+        }
+    });
+
+    const skillDocker = await prisma.skill.create({
+        data: {
+            id: 2,
+            name: "Docker",
+            createdBy: "seed"
+        }
+    });
+
+    const skillVisualStudio = await prisma.skill.create({
+        data: {
+            id: 3,
+            name: "Visual Studio",
+            createdBy: "seed"
+        },
+    });
+
+    const skillPython = await prisma.skill.create({
+        data: {
+            id: 4,
+            name: "Python",
+            createdBy: "seed"
+        }
+    });
+
+    const skillRLanguage = await prisma.skill.create({
+        data: {
+            id: 5,
+            name: "R Language",
+            createdBy: "seed"
+        }
+    });
+
+    const skillRStudio = await prisma.skill.create({
+        data: {
+            id: 6,
+            name: "RStudio",
+            createdBy: "seed"
+        }
+    });
+
+    const programmerPosition = await prisma.position.create({
+        data: {
+            id: 1,
+            name: ".NET Developer",
+            description: "Backend developer in .NET",
+            createdBy: "seed"
+        }
+    });
+
+    const dataAnalystPosition = await prisma.position.create({
+        data: {
+            id: 2,
+            name: "Data Analyst - Python",
+            description: "Data Analyst using Python",
+            createdBy: "seed"
+        }
+    });
+
+    const itDeptUnit = await prisma.unit.create({
+        data: {
+            id: 1,
+            name: "IT Department",
+            createdBy: "seed"
+        }
+    });
+
+    const webDevelopersUnit = await prisma.unit.create({
+        data: {
+            id: 2,
+            name: "Web Developers",
+            createdBy: "seed",
+            parentId: 1
+        }
+    });
+
+    const dataAnalystsUnit = await prisma.unit.create({
+        data: {
+            id: 3,
+            name: "Data Analysts",
+            createdBy: "seed",
+            parentId: 1
+        }
+    });
+
+    const user1 = await prisma.user.create({
+        data: {
+            id: 1,
             email: 'j.violin@test.com',
             firstname: "John",
             lastname: "Violin",
             description: "programmer",
+            positionId: 1,
             createdBy: "seed",
-            unit: {
-                create: {
-                    name: "Web Developers",
-                    createdBy: "seed",
-                    parent: {
-                        create: {
-                            id: 1,
-                            name: "IT Department",
-                            createdBy: "seed"
-                        }
-                    }
-                }
-            },
-            position: {
-                create: {
-                    name: ".NET Developer",
-                    description: "Backend developer in .NET",
-                    createdBy: "seed"
-                }
-            },
-            experience: {
-                create: {
-                    name: 'Software Developer at XYZ',
-                    description: "Developing software using .NET and React",
-                    startDate: moment().add(-3, "years").toDate(),
-                    endDate: moment().add(-1, "years").toDate(),
-                    isPrivate: false,
-                    createdBy: "seed"
-                },
-            },
-            competences: {
+            unitId: 2,
+            skills: {
                 create:
                     [
                         {
-                            name: ".NET",
+                            skillId: 1,
                             createdBy: "seed"
                         },
                         {
-                            name: "Docker",
+                            skillId: 2,
                             createdBy: "seed"
                         },
                         {
-                            name: "Visual Studio",
+                            skillId: 3,
                             createdBy: "seed"
                         }
                     ]
@@ -66,69 +137,29 @@ async function main() {
         },
     });
 
-    const user2 = await prisma.user.upsert({
-        where: { email: 'j.vengerberg@test.com' },
-        update: {},
-        create: {
+    const user2 = await prisma.user.create({
+        data: {
+            id: 2,
             email: 'j.vengerberg@test.com',
             firstname: "Jenna",
             lastname: "Vengerberg",
             description: "Data Analyst",
+            positionId: 2,
+            unitId: 3,
             createdBy: "seed",
-            unit: {
-                create: {
-                    name: "Data Analysts",
-                    createdBy: "seed",
-                    parent: {
-                        connect: {
-                            id: 1,
-                            name: "IT Department",
-                            createdBy: "seed"
-                        }
-                    }
-                }
-            },
-            position: {
-                create: {
-                    name: "Data Analyst - Python",
-                    description: "Data Analyst using Python",
-                    createdBy: "seed"
-                }
-            },
-            experience: {
+            skills: {
                 create:
                     [
                         {
-                            name: 'Python Developer at ABC',
-                            description: "Developing software using Python",
-                            startDate: moment().add(-6, "years").toDate(),
-                            endDate: moment().add(-4, "years").toDate(),
-                            isPrivate: false,
+                            skillId: 4,
                             createdBy: "seed"
                         },
                         {
-                            name: 'Data Analysts with R',
-                            description: "Creating data statistics using R language",
-                            startDate: moment().add(-4, "years").toDate(),
-                            endDate: moment().add(-3, "years").toDate(),
-                            isPrivate: false,
-                            createdBy: "seed"
-                        }
-                    ]
-            },
-            competences: {
-                create:
-                    [
-                        {
-                            name: "Python",
+                            skillId: 5,
                             createdBy: "seed"
                         },
                         {
-                            name: "R Language",
-                            createdBy: "seed"
-                        },
-                        {
-                            name: "RStudio",
+                            skillId: 6,
                             createdBy: "seed"
                         }
                     ]
@@ -136,9 +167,53 @@ async function main() {
         },
     });
 
+    const dotDevExp = await prisma.experience.create({
+        data: {
+            id: 1,
+            name: 'Software Developer at XYZ',
+            description: "Developing software using .NET and React",
+            startDate: moment().add(-3, "years").toDate(),
+            endDate: moment().add(-1, "years").toDate(),
+            isPrivate: false,
+            createdBy: "seed",
+            userId: 1
+        }
+    });
 
-    console.log({ user1, user2 });
+    const pythDevExp = await prisma.experience.create({
+        data: {
+            id: 2,
+            name: 'Python Developer at ABC',
+            description: "Developing software using Python",
+            startDate: moment().add(-6, "years").toDate(),
+            endDate: moment().add(-4, "years").toDate(),
+            isPrivate: false,
+            createdBy: "seed",
+            userId: 2
+        },
+    });
+
+    const dataAnExp = await prisma.experience.create({
+        data: {
+            id: 3,
+            name: 'Data Analysts with R',
+            description: "Creating data statistics using R language",
+            startDate: moment().add(-4, "years").toDate(),
+            endDate: moment().add(-3, "years").toDate(),
+            isPrivate: false,
+            createdBy: "seed",
+            userId: 2
+        }
+    });
+
+
+    console.log("Skills:", { skillDotnet, skillDocker, skillVisualStudio, skillPython, skillRLanguage, skillRStudio });
+    console.log("Positions:", { programmerPosition, dataAnalystPosition });
+    console.log("Units:", { itDeptUnit, webDevelopersUnit, dataAnalystsUnit });
+    console.log("Users:", { user1, user2 });
+    console.log("Experience:", { dotDevExp, pythDevExp, dataAnExp });
 }
+
 
 main()
     .catch((e) => {

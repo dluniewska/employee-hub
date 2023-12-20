63df2a95d7c4356fdf586475073bfee315aadcb1
Daria Łuniewska <luniewska.d@gmail.com>
2023-12-20 21:26:37 +0100

    updated user card, added random avatars, added shadcn (tailwind+ radix lib)

--- apps/web/src/components/ui/Card.tsx
+++ apps/web/src/components/ui/Card.tsx
@@ -1,13 +1,28 @@
 import { CardProps } from '../../types/props/types.cardProps'
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import ArrowForward from '../../assets/arrow-forward.svg?react'
+import { GetRandomAvatarPath } from '@/helpers/AvatarHelper';
 
-const Card = ({ user } : CardProps) => {
-  return (
-    <div className='rounded my-3 p-5 shadow-lg backdrop-blur-sm bg-white/25'>
-        <h3 className='text-xl mb-1 font-bold'>{`${user.firstname} ${user.lastname}`}</h3>
-        <h2 className='text-lg mb-3'>{user.position.name}</h2>
-        <h2 className='text-sm'>{user.unit.name}</h2>
-    </div>
-  )
+const Card = ({ user }: CardProps) => {
+    let path = GetRandomAvatarPath();
+    return (
+        <div className='rounded my-3 px-2 py-6 shadow-lg backdrop-blur-sm bg-white/25 flex flex-row'>
+            <div className='basis-1/6 m-auto flex justify-center'>
+                <Avatar>
+                    <AvatarImage src={path} />
+                    <AvatarFallback>{Array.from(user.firstname)[0]}{Array.from(user.lastname)[0]}</AvatarFallback>
+                </Avatar>
+            </div>
+            <div className='basis-4/6'>
+                <h3 className='text-xl mb-1 font-bold'>{`${user.firstname} ${user.lastname}`}</h3>
+                <h2 className='text-lg mb-3'>{user.position.name}</h2>
+                <h2 className='text-sm'>{user.unit.name}</h2>
+            </div>
+            <div className='basis-1/6 m-auto flex align-middle justify-center'>
+                <ArrowForward />
+            </div>
+        </div>
+    )
 }
 
 export default Card
---
+++ apps/web/src/helpers/AvatarHelper.tsx
@@ -0,0 +1,13 @@
+const paths = [
+    './src/assets/avatars/cat.svg',
+    './src/assets/avatars/chicken.svg',
+    './src/assets/avatars/hotdog.svg',
+    './src/assets/avatars/pug.svg',
+    './src/assets/avatars/red-cat.svg',
+    './src/assets/avatars/tiger.svg',
+    './src/assets/avatars/snail.svg',
+]
+
+export function GetRandomAvatarPath(): string {
+    return paths[Math.floor(Math.random() * paths.length)];
+}

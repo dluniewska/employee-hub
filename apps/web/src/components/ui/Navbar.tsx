5f40f04f5a6023bd81a4030621a943db2e1c5567
Daria Łuniewska <luniewska.d@gmail.com>
2024-01-06 15:01:48 +0100

    updated common scripts, changed divs for buttons in links

--- apps/web/src/components/ui/Navbar.tsx
+++ apps/web/src/components/ui/Navbar.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import SearchIcon from '@/assets/search-icon.svg?react'
+import { Button } from "@/components/ui/button"
 
 const Navbar = ({ setSearchString }: { setSearchString: React.Dispatch<React.SetStateAction<string>> }) => {
 
@@ -18,8 +19,8 @@ const Navbar = ({ setSearchString }: { setSearchString: React.Dispatch<React.Set
             <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
                 <div className="relative flex flex-wrap h-16 items-center justify-between align-middle">
 
-                    <div onClick={() => handleLogoClick()} className="text-xl font-sans font-bold subpixel-antialiased flex-1">
-                        EmployeesHub
+                    <div className="flex-1">
+                        <Button onClick={() => handleLogoClick()} variant="hidden" className='text-xl font-sans font-bold subpixel-antialiased'>EmployeesHub</Button>
                     </div>
 
                     <div className="xl:w-96 flex-1 flex-row min-w-fit">
@@ -32,15 +33,15 @@ const Navbar = ({ setSearchString }: { setSearchString: React.Dispatch<React.Set
                                 aria-describedby="button-search"
                                 onChange={(e) => setSearchString(e.target.value)} />
                             <span
-                                className="input-group-text flex items-center whitespace-nowrap rounded px-3 py-1.5 text-center hover:translate-x-2 hover:scale-110 duration-150"
+                                className="input-group-text flex items-center whitespace-nowrap rounded px-3 py-1.5 text-center hover:scale-110 duration-150"
                                 id="button-search">
                                 <SearchIcon className='fill-current' />
                             </span>
                         </div>
                     </div>
 
-                    <div onClick={() => handleLoginClick()} className="w-full flex-1 text-end">
-                        <p>Zaloguj</p>
+                    <div className="w-full flex-1 text-end">
+                        <Button onClick={() => handleLoginClick()} variant="outline">Zaloguj</Button>
                     </div>
                 </div>
             </div>
